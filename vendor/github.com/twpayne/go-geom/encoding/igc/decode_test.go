6ffdea88b545ffecd043984a2a52b3b3b0a1a8aa
thisisaaronland <devnull@localhost>
2019-03-15 11:33:09 -0700

    update vendor-deps

--- vendor/github.com/twpayne/go-geom/encoding/igc/decode_test.go
+++ vendor/github.com/twpayne/go-geom/encoding/igc/decode_test.go
@@ -2,9 +2,9 @@ package igc
 
 import (
 	"bytes"
-	"reflect"
 	"testing"
 
+	"github.com/d4l3k/messagediff"
 	"github.com/twpayne/go-geom"
 )
 
@@ -18,6 +18,9 @@ func TestDecode(t *testing.T) {
 				"HFDTE151115\r\n" +
 				"B1316284654230N00839078EA0147801630\r\n",
 			t: &T{
+				Headers: []Header{
+					{Source: "F", Key: "DTE", KeyExtra: "", Value: "151115"},
+				},
 				LineString: geom.NewLineString(geom.Layout(5)).MustSetCoords([]geom.Coord{
 					{8.6513, 46.90383333333333, 1630, 1447593388, 1478},
 				}),
@@ -29,6 +32,9 @@ func TestDecode(t *testing.T) {
 				"I033638FXA3940SIU4141TDS\r\n" +
 				"B1053525151892N00203986WA0017900275000108\r\n",
 			t: &T{
+				Headers: []Header{
+					{Source: "F", Key: "DTE", KeyExtra: "", Value: "020613"},
+				},
 				LineString: geom.NewLineString(geom.Layout(5)).MustSetCoords([]geom.Coord{
 					{-2.0664333333333333, 51.864866666666664, 275, 1370170432.8, 179},
 				}),
@@ -40,14 +46,35 @@ func TestDecode(t *testing.T) {
 				"I033637LAD3839LOD4040TDS\r\n" +
 				"B1146174031985N00726775WA010040114912340",
 			t: &T{
+				Headers: []Header{
+					{Source: "F", Key: "DTE", KeyExtra: "", Value: "100810"},
+				},
 				LineString: geom.NewLineString(geom.Layout(5)).MustSetCoords([]geom.Coord{
 					{-7.446255666666667, 40.53308533333333, 1149, 1281440777, 1004},
 				}),
 			},
 		},
+		{
+			s: "AXGD Flymaster LiveSD  SN03142  SW1.07b\r\n" +
+				"HFDTEDATE:220418,01\r\n" +
+				"B1316284654230N00839078EA0147801630\r\n",
+			t: &T{
+				Headers: []Header{
+					{Source: "F", Key: "DTE", KeyExtra: "DATE", Value: "220418,01"},
+				},
+				LineString: geom.NewLineString(geom.Layout(5)).MustSetCoords([]geom.Coord{
+					{8.6513, 46.90383333333333, 1630, 1524402988, 1478},
+				}),
+			},
+		},
 	} {
-		if got, err := Read(bytes.NewBufferString(tc.s)); err != nil || !reflect.DeepEqual(tc.t, got) {
-			t.Errorf("Read(...(%#v)) == %#v, %v, want nil, %#v", tc.s, got, err, tc.t)
+		got, err := Read(bytes.NewBufferString(tc.s))
+		diff, equal := "", true
+		if err == nil {
+			diff, equal = messagediff.PrettyDiff(tc.t, got)
+		}
+		if err != nil || !equal {
+			t.Errorf("Read(...(%#v)) == %#v, %v, want nil, %#v\n%s", tc.s, got, err, tc.t, diff)
 		}
 	}
 }
@@ -71,6 +98,8 @@ func TestDecodeHeaders(t *testing.T) {
 				"HFFTYFRTYPE:FLYTEC,5020\r\n",
 			t: &T{
 				Headers: []Header{
+					{Source: "F", Key: "DTE", KeyExtra: "", Value: "210407"},
+					{Source: "F", Key: "FXA", KeyExtra: "", Value: "100"},
 					{Source: "F", Key: "PLT", KeyExtra: "PILOT", Value: "Tom Payne"},
 					{Source: "F", Key: "GTY", KeyExtra: "GLIDERTYPE", Value: "Gradient Aspen"},
 					{Source: "F", Key: "GID", KeyExtra: "GLIDERID", Value: "G12242505057"},
@@ -82,9 +111,49 @@ func TestDecodeHeaders(t *testing.T) {
 				},
 			},
 		},
+		{
+			s: "AXTR7F094645CA98\r\n" +
+				"HFDTE220418\r\n" +
+				"HFFXA100\r\n" +
+				"HFPLTPILOTINCHARGE:\r\n" +
+				"HFCM2CREW2:\r\n" +
+				"HFGTYGLIDERTYPE:\r\n" +
+				"HFGIDGLIDERID:\r\n" +
+				"HFDTM100GPSDATUM:WGS84\r\n" +
+				"HFRFWFIRMWAREREVISION:XC_Tracer_II_R01.1\r\n" +
+				"HFRHWHARDWAREVERSION:1.0\r\n" +
+				"HFFTYFRTYPE:XC_Tracer_II\r\n" +
+				"HFGPSRECV:u-Blox,MAX-8Q,22,9999\r\n" +
+				"HFPRSPRESSALTSENSOR:Measurement Specialities,MS5637,9999\r\n" +
+				"HFALG:ELL\r\n" +
+				"HFALP:ISA\r\n",
+			t: &T{
+				Headers: []Header{
+					{Source: "F", Key: "DTE", KeyExtra: "", Value: "220418"},
+					{Source: "F", Key: "FXA", KeyExtra: "", Value: "100"},
+					{Source: "F", Key: "PLT", KeyExtra: "PILOTINCHARGE", Value: ""},
+					{Source: "F", Key: "CM2", KeyExtra: "CREW2", Value: ""},
+					{Source: "F", Key: "GTY", KeyExtra: "GLIDERTYPE", Value: ""},
+					{Source: "F", Key: "GID", KeyExtra: "GLIDERID", Value: ""},
+					{Source: "F", Key: "DTM", KeyExtra: "100GPSDATUM", Value: "WGS84"},
+					{Source: "F", Key: "RFW", KeyExtra: "FIRMWAREREVISION", Value: "XC_Tracer_II_R01.1"},
+					{Source: "F", Key: "RHW", KeyExtra: "HARDWAREVERSION", Value: "1.0"},
+					{Source: "F", Key: "FTY", KeyExtra: "FRTYPE", Value: "XC_Tracer_II"},
+					{Source: "F", Key: "GPS", KeyExtra: "RECV", Value: "u-Blox,MAX-8Q,22,9999"},
+					{Source: "F", Key: "PRS", KeyExtra: "PRESSALTSENSOR", Value: "Measurement Specialities,MS5637,9999"},
+					{Source: "F", Key: "ALG", KeyExtra: "", Value: "ELL"},
+					{Source: "F", Key: "ALP", KeyExtra: "", Value: "ISA"},
+				},
+			},
+		},
 	} {
-		if got, err := Read(bytes.NewBufferString(tc.s)); err != nil || !reflect.DeepEqual(tc.t.Headers, got.Headers) {
-			t.Errorf("Read(...(%#v)) == %#v, %v, want nil, %#v", tc.s, got, err, tc.t)
+		got, err := Read(bytes.NewBufferString(tc.s))
+		diff, equal := "", true
+		if err == nil {
+			diff, equal = messagediff.PrettyDiff(tc.t.Headers, got.Headers)
+		}
+		if err != nil || !equal {
+			t.Errorf("Read(...(%#v)) == %#v, %v, want nil, %#v\n%s", tc.s, got, err, tc.t, diff)
 		}
 	}
 }
--- vendor/github.com/twpayne/go-geom/go.mod
+++ vendor/github.com/twpayne/go-geom/go.mod
@@ -1,3 +1,8 @@
 module github.com/twpayne/go-geom
 
-require github.com/twpayne/go-kml v1.0.0
+require (
+	github.com/DATA-DOG/go-sqlmock v1.3.2
+	github.com/d4l3k/messagediff v1.2.1
+	github.com/lib/pq v1.0.0
+	github.com/twpayne/go-kml v1.0.0
+)
